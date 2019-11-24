32e7b414b98ecda05dc1792998d17f9e5c8b9870
YanlinWangWang <chinese1student@gmail.com>
2019-11-23 18:48:21 -0800

    meta为源信息文件夹

---
+++ meta/filemeta.go
@@ -0,0 +1,28 @@
+package meta
+
+// FileMeta : 文件元信息结构
+type FileMeta struct {
+	FileSha1 string
+	FileName string
+	FileSize int64
+	Location string
+	UploadAt string
+}
+
+//第一个元素为hash值 hash找到对应文件结构体
+var fileMetas map[string]FileMeta
+
+
+func init() {
+	fileMetas = make(map[string]FileMeta)
+}
+
+// UpdateFileMeta : 新增/更新文件元信息
+func UpdateFileMeta(fmeta FileMeta) {
+	fileMetas[fmeta.FileSha1] = fmeta
+}
+
+// GetFileMeta : 通过sha1值获取文件的元信息对象
+func GetFileMeta(fileSha1 string) FileMeta {
+	return fileMetas[fileSha1]
+}
