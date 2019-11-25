cd86736f1e93443075c76bee0413acf7d55f0a3f
YanlinWangWang <chinese1student@gmail.com>
2019-11-24 21:14:46 -0800

    添加几个API“

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 
 	"./handler"
 )
-
+//主函数当中有路由信息
 func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/success", handler.UploadSucHandler)
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
+	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
+	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("服务器错误%s", err.Error())
